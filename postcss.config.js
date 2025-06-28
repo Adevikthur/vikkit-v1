646c0e95fdd486538aebfca1607e5c66402d4e77
Julius Adekanbi Asaolu <adevikthur@gmail.com>
2025-06-28 15:43:31 -0500

    autoprefixer fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 // PostCSS configuration file for the Vikkit UI Components project
 // PostCSS is a tool for transforming CSS with JavaScript plugins
 
-export default {
-  // Configure PostCSS plugins
-  plugins: {
-    // Ensures cross-browser compatibility for modern CSS features
-    autoprefixer: {},
-  },
-};
+// export default {
+//   // Configure PostCSS plugins
+//   plugins: {
+//     // Ensures cross-browser compatibility for modern CSS features
+//     autoprefixer: {},
+//   },
+// };
